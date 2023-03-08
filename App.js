8da4bff21cd677fea5ae0bb53c28e6563f9ab7a7
LilAguh <aguezeochoa@gmail.com>
2023-03-08 10:20:51 -0300

    entra en funcionamiento la camara delantera, con problemas de resolucion

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library'
+import React, { useEffect, useRef, useState } from 'react';
 
 export default function App() {
+  const [hasCameraPermissions, setHasCameraPermission] = useState(null)
+  const [image, setImage] = useState(null)
+  const [type, setType] = useState(Camera.Constants.Type.front)
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off)
+  const cameraRef = useRef(null)
+
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync()
+      const cameraStatus = await Camera.requestCameraPermissionsAsync()
+      setHasCameraPermission(cameraStatus.status === 'granted')
+    })();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        style={styles.camera}
+        type={type}
+        flashMode={flash}
+        ref={cameraRef}>
+        <Text style={styles.text}>Hello</Text>
+      </Camera>
     </View>
   );
 }
@@ -13,8 +34,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0ABAB5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 120,
+    color: 'lightblue',
+    // backgroundColor: '#000000',
+    height: '100%',
+    width: '100%',
+    textAlignVertical: 'center',
+    textAlign: 'center'
+  },
+  camera: {
+    flex: 1,
+    borderRadius: 20,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
